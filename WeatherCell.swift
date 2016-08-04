d5e46fdfdc6ffcfa5c8e94717546d6a1aa864765
esscbee <github@esscb.com>
2016-08-04 12:45:18 -0700

    fix text sizing

--- WeatherCell.swift
+++ WeatherCell.swift
@@ -16,6 +16,21 @@ class WeatherCell : UITableViewCell {
     
     func loadItem(weather: String?, time: String?, maybeImage: NSData?, maybeLocation: String?) {
         weatherLabel.text = weather ?? "No Weather"
+        while true {
+            guard let font = weatherLabel.font else {
+                break
+            }
+            let text = weatherLabel.text! as NSString
+            
+            let atts = [NSFontAttributeName: font]
+            let r = text.boundingRectWithSize(CGSizeMake(320, 2000), options: .UsesLineFragmentOrigin, attributes: atts, context: nil)
+            
+            if r.width < frame.width {
+                break
+            }
+
+            weatherLabel.font = UIFont(name: font.fontName, size: 0.9 * font.pointSize)
+        }
         timeLabel.text = time ?? "No Time"
         locationLabel.text = maybeLocation ?? ""
         if let image = maybeImage {
